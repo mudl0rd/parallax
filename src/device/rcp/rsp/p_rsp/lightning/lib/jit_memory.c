1097bb470f1389de748489952285265323f9d86a
mudlord <mudlord@protonmail.com>
2022-08-21 09:11:23 +1000

    Make it somewhat compile on Linux, untested.

--- src/device/rcp/rsp/p_rsp/lightning/lib/jit_memory.c
+++ src/device/rcp/rsp/p_rsp/lightning/lib/jit_memory.c
@@ -19,13 +19,13 @@
 
 #include <lightning.h>
 #include <lightning/jit_private.h>
-#include <mman.h>
+#include <sys/mman.h>
 
 /*
  * Prototypes
  */
 static void *jit_default_alloc_func(size_t);
-static void *jit_default_realloc_func(void *, size_t);
+static void *jit_default_realloc_func(void*, size_t);
 static void jit_default_free_func(void *);
 
 /*
@@ -38,75 +38,86 @@ static jit_free_func_ptr jit_free_ptr = jit_default_free_func;
 /*
  * Implementation
  */
-jit_pointer_t jit_memcpy(jit_pointer_t dst, const void *src, jit_word_t size)
+jit_pointer_t
+jit_memcpy(jit_pointer_t dst, const void * src, jit_word_t size)
 {
-	if (size)
-		return (memcpy(dst, src, size));
-	return (dst);
+    if (size)
+	return (memcpy(dst, src, size));
+    return (dst);
 }
 
-jit_pointer_t jit_memmove(jit_pointer_t dst, const void *src, jit_word_t size)
+jit_pointer_t
+jit_memmove(jit_pointer_t dst, const void *src , jit_word_t size)
 {
-	if (size)
-		return (memmove(dst, src, size));
-	return (dst);
+    if (size)
+	return (memmove(dst, src, size));
+    return (dst);
 }
 
-void jit_set_memory_functions(jit_alloc_func_ptr alloc_ptr, jit_realloc_func_ptr realloc_ptr,
-                              jit_free_func_ptr free_ptr)
+void
+jit_set_memory_functions(jit_alloc_func_ptr alloc_ptr,
+			 jit_realloc_func_ptr realloc_ptr,
+			 jit_free_func_ptr free_ptr)
 {
-	if (alloc_ptr == NULL)
-		alloc_ptr = jit_default_alloc_func;
-	if (realloc_ptr == NULL)
-		realloc_ptr = jit_default_realloc_func;
-	if (free_ptr == NULL)
-		free_ptr = jit_default_free_func;
-	jit_alloc_ptr = alloc_ptr;
-	jit_realloc_ptr = realloc_ptr;
-	jit_free_ptr = free_ptr;
+    if (alloc_ptr == NULL)
+	alloc_ptr = jit_default_alloc_func;
+    if (realloc_ptr == NULL)
+	realloc_ptr = jit_default_realloc_func;
+    if (free_ptr == NULL)
+	free_ptr = jit_default_free_func;
+    jit_alloc_ptr = alloc_ptr;
+    jit_realloc_ptr = realloc_ptr;
+    jit_free_ptr = free_ptr;
 }
 
-void jit_get_memory_functions(jit_alloc_func_ptr *alloc_ptr, jit_realloc_func_ptr *realloc_ptr,
-                              jit_free_func_ptr *free_ptr)
+void
+jit_get_memory_functions(jit_alloc_func_ptr *alloc_ptr,
+			 jit_realloc_func_ptr *realloc_ptr,
+			 jit_free_func_ptr *free_ptr)
 {
-	*alloc_ptr = jit_alloc_ptr;
-	*realloc_ptr = jit_realloc_ptr;
-	*free_ptr = jit_free_ptr;
+    *alloc_ptr = jit_alloc_ptr;
+    *realloc_ptr = jit_realloc_ptr;
+    *free_ptr = jit_free_ptr;
 }
 
-void jit_alloc(jit_pointer_t *ptr, jit_word_t size)
+void
+jit_alloc(jit_pointer_t *ptr, jit_word_t size)
 {
-	*ptr = (*jit_alloc_ptr)(size);
-	memset(*ptr, 0, size);
+    *ptr = (*jit_alloc_ptr)(size);
+    memset(*ptr, 0, size);
 }
 
-void jit_realloc(jit_pointer_t *ptr, jit_word_t old_size, jit_word_t new_size)
+void
+jit_realloc(jit_pointer_t *ptr, jit_word_t old_size, jit_word_t new_size)
 {
-	*ptr = (*jit_realloc_ptr)(*ptr, new_size);
-	if (old_size < new_size)
-		memset((jit_int8_t *)*ptr + old_size, 0, new_size - old_size);
+    *ptr = (*jit_realloc_ptr)(*ptr, new_size);
+    if (old_size < new_size)
+	memset((jit_int8_t*)*ptr + old_size, 0, new_size - old_size);
 }
 
-void jit_free(jit_pointer_t *ptr)
+void
+jit_free(jit_pointer_t *ptr)
 {
-	if (*ptr)
-	{
-		(*jit_free_ptr)(*ptr);
-		*ptr = NULL;
-	}
+    if (*ptr) {
+	(*jit_free_ptr)(*ptr);
+	*ptr = NULL;
+    }
 }
 
-static void *jit_default_alloc_func(size_t size)
+static void *
+jit_default_alloc_func(size_t size)
 {
-	return (malloc(size));
+    return (malloc(size));
 }
 
-static void *jit_default_realloc_func(void *ptr, size_t size)
+static void *
+jit_default_realloc_func(void *ptr, size_t size)
 {
-	return (realloc(ptr, size));
+    return (realloc(ptr, size));
 }
 
-static void jit_default_free_func(void *ptr)
+static void
+jit_default_free_func(void *ptr)
 {
-	free(ptr);
+    free(ptr);
 }
